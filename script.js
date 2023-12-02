75f0a9cd26fb36c3c8bd35f1310c2c3c5a0b41ae
MisterPhoenix <150902093+MisterPhoenix@users.noreply.github.com>
2023-12-02 18:21:53 +0000

    récup champ par ID et info a remplir en JS

--- script.js
+++ script.js
@@ -66,6 +66,8 @@ class Tdip{
     }
 }
 
+// création des constantes pour chaques champs
+
 const poste1 = new Poste();
 const tel1 = new Tel();
 const email1 = new Email();
@@ -77,3 +79,17 @@ const texp1 = new Texp();
 const pdip1 = new Pdip();
 const sdip1 = new Sdip();
 const tdip = new Tdip();
+
+// récupération des champs à remplir par l'ID avec les informations à chercher côté JavaScript
+
+document.getElementById("poste").innerHTML = poste1.poste;
+document.getElementById("tel").innerHTML = tel1.tel;
+document.getElementById("email").innerHTML = email1.email;
+document.getElementById("pss").innerHTML = pss1.pss;
+document.getElementById("sss").innerHTML = sss1.sss;
+document.getElementById("pexp").innerHTML = pexp1.pexp;
+document.getElementById("sexp").innerHTML = sexp1.sexp;
+document.getElementById("texp").innerHTML = texp1.texp;
+document.getElementById("pdip").innerHTML = pdip1.pdip;
+document.getElementById("sdip").innerHTML = sdip1.sdip;
+document.getElementById("tdip").innerHTML = tdip1.tdip;
