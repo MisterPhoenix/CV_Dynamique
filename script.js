3bdbc8ba65d8bd81fb7edec7066e88f5bbed6b9d
MisterPhoenix <150902093+MisterPhoenix@users.noreply.github.com>
2023-12-02 10:04:59 +0000

    création de toutes les class

--- script.js
+++ script.js
@@ -5,3 +5,63 @@ class Poste{
         this.poste = poste;
     }
 }
+
+class Tel{
+    constructor(tel){
+        this.tel = tel;
+    }
+}
+
+class Email{
+    constructor(email){
+        this.email = email;
+    }
+}
+
+class Pss{
+    constructor(pss){
+        this.pss = pss;
+    }
+}
+
+class Sss{
+    constructor(sss){
+        this.sss = sss;
+    }
+}
+
+class Pexp{
+    constructor(pexp){
+        this.pexp = pexp;
+    }
+}
+
+class Sexp{
+    constructor(sexp){
+        this.sexp = sexp;
+    }
+}
+
+class Texp{
+    constructor(texp){
+        this.texp = texp;
+    }
+}
+
+class Pdip{
+    constructor(pdip){
+        this.pdip = pdip;
+    }
+}
+
+class Sdip{
+    constructor(sdip){
+        this.sdip = sdip;
+    }
+}
+
+class Tdip{
+    constructor(tdip){
+        this.tdip = tdip;
+    }
+}
